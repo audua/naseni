a41b1bd8a367458494d510de1e525c26040ee67e
auduab <auduab@gmail.com>
2023-08-14 13:25:12 +0100

    changes to allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-lg9u0f*ml)req)yo@@0$vd!q%hep__h!om@$6d+lqe$-pqleum
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["naseni-auduab.b4a.run"]
+ALLOWED_HOSTS = [
+  'localhost',
+  '127.0.0.1',
+  'naseni-auduab.b4a.run']
 
 
 # Application definition
